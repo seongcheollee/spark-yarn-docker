c537b984657b082db4c97150847ba144dc9d4af8
seongcheollee <jhk6520736@gmail.com>
2023-11-05 02:30:31 +0900

    initial

---
+++ docker-build.sh
@@ -0,0 +1,14 @@
+#docker-compose down
+
+#docker build --platform linux/amd64  -t language-base ./language-base
+docker build --platform linux/amd64  -t hadoop-base ./hadoop-base
+docker build --platform linux/amd64  -t spark-base ./spark-base
+
+docker build --platform linux/amd64  -t namenode ./namenode
+docker build --platform linux/amd64  -t datanode ./datanode
+docker build --platform linux/amd64  -t resourcemanager ./resourcemanager
+docker build --platform linux/amd64  -t yarntimelineserver ./yarntimelineserver
+docker build --platform linux/amd64  -t sparkhistoryserver ./sparkhistoryserver
+docker build --platform linux/amd64  -t zeppelin ./zeppelin
+
+#docker-compose up -d
