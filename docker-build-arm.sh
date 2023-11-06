771999cc9449a7ff645681271b4bda0cde3ace90
seongcheollee <jhk6520736@gmail.com>
2023-11-06 19:08:38 +0900

    주석 추가

--- docker-build-arm.sh
+++ docker-build-arm.sh
@@ -6,6 +6,6 @@ docker build  -t spark-base ./spark-base
 docker build  -t namenode ./namenode
 docker build  -t datanode ./datanode
 docker build  -t resourcemanager ./resourcemanager
-# docker build  -t yarntimelineserver ./yarntimelineserver
+# docker build  -t yarntimelineserver ./yarntimelineserver arm으로 실행시 문제 발생
 docker build  -t sparkhistoryserver ./sparkhistoryserver
 docker build  -t zeppelin ./zeppelin
